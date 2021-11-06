ee5ee5e68d2aa544ae0bb4d195460d90bc38f585
DeanDal1000 <dalzelldean@gmail.com>
2021-11-06 15:54:54 +0000

    Hello

---
+++ src/styles/Theme.js
@@ -0,0 +1,8 @@
+// import { createTheme } from '@material-ui/core';
+// import { purple } from '@mui/material/colors';
+
+// export const theme = createTheme({
+//   palette: {
+//     primary: purple[500],
+//   },
+// });
